3770675e7accaebcdfbe70f4609cc7c46396504a
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2024-10-11 21:00:55 +0300

    header mobile styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,17 +12,27 @@ const navItems = [
 export const Header = () => {
   const [visible, setVisible] = useState(true);
 
+  const viewport = window.innerWidth;
+
   const handleScroll = useCallback(() => {
     const currentScrollPosition = document.documentElement.scrollTop;
 
-    if (currentScrollPosition > 0) {
+    if (viewport > 767 && currentScrollPosition > 0) {
       setVisible(false);
     }
-  }, []);
+  }, [viewport]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
   }, [handleScroll]);
+
+  useEffect(() => {
+    if (viewport < 768 && visible) {
+      document.body.classList.add('noscroll');
+    } else {
+      document.body.classList.remove('noscroll');
+    }
+  }, [visible, viewport]);
 
   return (
     <>
@@ -30,6 +40,17 @@ export const Header = () => {
         className={`header ${visible ? 'header__visible' : 'header__hidden'}`}
       >
         <div className="container">
+          <button
+            className={`header-exit_btn ${
+              visible ? 'header-btn__visible' : 'header-btn__hidden'
+            }`}
+            onClick={() => setVisible(false)}
+          >
+            <svg className="header-exit_btn__icon">
+              <use href={`${Icons}#exit`} width="100%" height="100%"></use>
+            </svg>
+          </button>
+
           <a>
             <svg className="header-logo">
               <use href={`${Icons}#logo`} width="100%" height="100%"></use>
@@ -40,7 +61,11 @@ export const Header = () => {
             <ul className="header-nav__list">
               {navItems.map((item, index) => (
                 <li className="header-nav__item" key={index}>
-                  <a href={item.href} className="header-nav__link">
+                  <a
+                    href={item.href}
+                    className="header-nav__link"
+                    onClick={() => setVisible(false)}
+                  >
                     <span className="header-nav__bracket">{'[ '}</span>
                     <span className="header-nav__bracket">{'] '}</span>
                     <span className="header-nav__text">{item.text}</span>
@@ -50,30 +75,43 @@ export const Header = () => {
             </ul>
           </nav>
 
-          <ul className="header-social__list">
-            <li className="header-social__item">
-              <a href="https://www.instagram.com/musick.project" target="blank">
-                <svg className="header-social__icon">
-                  <use
-                    href={`${Icons}#instagram`}
-                    width="100%"
-                    height="100%"
-                  ></use>
-                </svg>
-              </a>
-            </li>
-            <li className="header-social__item">
-              <a href="https://www.tiktok.com/@musick.project" target="blank">
-                <svg className="header-social__icon">
-                  <use
-                    href={`${Icons}#tiktok`}
-                    width="100%"
-                    height="100%"
-                  ></use>
-                </svg>
-              </a>
-            </li>
-          </ul>
+          <div className="header-social__list">
+            <a
+              href="https://www.instagram.com/musick.project"
+              target="blank"
+              onClick={() => setVisible(false)}
+            >
+              <svg className="header-social__icon">
+                <use
+                  href={`${Icons}#instagram`}
+                  width="100%"
+                  height="100%"
+                ></use>
+              </svg>
+            </a>
+
+            <a
+              href="https://www.tiktok.com/@musick.project"
+              target="blank"
+              onClick={() => setVisible(false)}
+            >
+              <svg className="header-social__icon">
+                <use href={`${Icons}#tiktok`} width="100%" height="100%"></use>
+              </svg>
+            </a>
+
+            <a
+              href="https://open.spotify.com/playlist/4g4XawS5tTHpPTGFHkEEGY?si=ANYIXh9mROSLF9fB-cRwLg&pi=e-5M3ULAeYRNiP"
+              target="blank"
+              onClick={() => setVisible(false)}
+            >
+              <svg className="header-social__icon">
+                <use href={`${Icons}#spotify`} width="100%" height="100%"></use>
+              </svg>
+            </a>
+
+            <span className="header-social__span">say hello!</span>
+          </div>
         </div>
       </header>
 
@@ -83,7 +121,10 @@ export const Header = () => {
         }`}
         onClick={() => setVisible(true)}
       >
-        V
+        <span>V</span>
+        <svg className="header-btn__icon">
+          <use href={`${Icons}#burger`} width="100%" height="100%"></use>
+        </svg>
       </button>
     </>
   );
